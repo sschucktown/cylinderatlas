58b5fd2c26d5cfb2ea688ed4914577a88c3216bf
Scott <scottmsandie@gmail.com>
2026-09-24 20:41:08 -0400

    Fix Nuxt 4 root type import

--- app/plugins/supabase.client.ts
+++ app/plugins/supabase.client.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import type { Database } from '~/types/database'
+import type { Database } from '~~/types/database'
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
