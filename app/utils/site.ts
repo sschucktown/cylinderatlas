efad9d17dbf4279d1d1d2769cfe05796df126e54
Scott <scottmsandie@gmail.com>
2026-09-25 11:56:42 -0400

    Use production domain for canonical URLs

--- app/utils/site.ts
+++ app/utils/site.ts
@@ -1,4 +1,4 @@
-export const SITE_URL = 'https://cylinderatlas.vercel.app'
+export const SITE_URL = 'https://www.cylinderatlas.com'
 
 export function canonicalUrl(path = '/') {
   return new URL(path, SITE_URL).toString()
